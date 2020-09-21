0a3bc4af6d5480433237beaf754b4d680a6c7683
David Gold <davidgold@hey.com>
2020-09-21 14:19:51 -0400

    some form in place

--- med-cabinet/src/components/TreatmentForm.js
+++ med-cabinet/src/components/TreatmentForm.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import Checkbox from '@material-ui/core/Checkbox'
+import { FormControlLabel } from '@material-ui/core';
+import { ADD_TREATMENT } from '../store/actions'
 
-const TreatmentForm = () => {
+const initialValues = [];
+
+const TreatmentForm = (props) => {
+
+    const [formValues, setFormValues] = useState(initialValues)
+
+    // const submitHandler = (evt) => {
+    //     evt.preventDefault;
+    //     const {name, value} = evt.target;
+    //     props.ADD_TREATMENT(formValues);
+    // }
 
     return (
         <div>
             <h1>What do you want to treat?</h1>
             <form onSubmit={null} >
-                <input
-                className='form-input'
-                name='sleepy' // to make dynamic
-                type='checkbox'
-                value='newTitleText' // to make dynamic
-                onChange={null} // to add in click handler
-                />
-
-                <input
-                className='form-input'
-                name='sleepy' // to make dynamic
-                type='checkbox'
-                value='newTitleText' // to make dynamic
-                onChange={null} // to add in click handler
+                <FormControlLabel
+                control={
+                    <Checkbox
+                    className='form-input'
+                    name='sleepy' // to make dynamic
+                    type='checkbox'
+                    check={null} // add state
+                    value='newTitleText' // to make dynamic
+                    onChange={null} // to add in click handler
+                    />} 
+                label='Test'    
                 />
 
                 <button>Suggest Strains</button>
@@ -28,4 +39,10 @@ const TreatmentForm = () => {
     )
 }
 
-export default TreatmentForm;
+const mapStateToProps = state => {
+    return {
+        formSelections: state.formSelections, 
+    }
+}
+
+export default connect(mapStateToProps, {ADD_TREATMENT})(TreatmentForm);
